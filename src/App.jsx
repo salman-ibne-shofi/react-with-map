94460f456a02dabb03cd4b47d05e6cd3bc90f14a
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-09-05 18:47:42 +0600

    display a loaction and mnaker on a google map

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import GoogleMapReact from "google-map-react";
+import { googleAPIKey } from "./googleAPIKey";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+function App() {
+	const defaultProps = {
+		center: {
+			lat: 10.99835602,
+			lng: 77.01502627,
+		},
+		zoom: 11,
+	};
+
+	return (
+		<>
+			<div style={{ height: "100vh", width: "100%" }}>
+				<GoogleMapReact
+					bootstrapURLKeys={{ key: googleAPIKey }}
+					defaultCenter={defaultProps.center}
+					defaultZoom={defaultProps.zoom}
+				>
+					<AnyReactComponent
+						lat={59.955413}
+						lng={30.337844}
+						text="My Marker"
+					/>
+				</GoogleMapReact>
+			</div>
+		</>
+	);
+}
+
+export default App;
